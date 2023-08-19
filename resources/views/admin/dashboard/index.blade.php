84b03bd003205935bc6f46b0b24dab93a90b0e39
zakyrama312 <zakyrama312@gmail.com>
2023-08-19 12:20:06 +0700

    first commit

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,18 @@
+    @extends('admin.layouts.main')
+        {{-- content --}}
+        @section('content')
+        <h1 class="h3 mb-3"><strong>Analytics</strong> Dashboard</h1>
+
+        <div class="row">
+            <div class="col-12 col-lg-12 col-xxl-12 d-flex">
+                <div class="card flex-fill">
+                    <div class="card-header">
+                        <h5 class="card-title mb-0">Latest Projects</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endsection
+        {{-- end content --}}
+
+				
